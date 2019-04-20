b76d30a8c9a031e7e2f596afccaf42ef2a83467e
Zkinsk <zoolunk@gmail.com>
2019-04-20 14:08:13 -0400

    update portfolio item - images - text - title

--- client/src/components/PortfolioFormComponent.js
+++ client/src/components/PortfolioFormComponent.js
@@ -9,91 +9,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
-
-
-
-  // function PortolfioImages(props){
-    // console.log(props);
-
-
-
-
-  // export function ImageUpdateForm (props) {
-  //   return (
-  //     <>
-  //       <Form>
-  //         <Form.Group controlId="exampleForm.ControlInput1">
-  //           <Row className="my-2">
-  //             <Col xs={6}>
-  //             <Form.Label>First Name</Form.Label>
-  //               <Form.Control
-  //                 type="text"
-  //                 placeholder="First name"
-  //                 value={props.userInfo.firstname}
-  //                 onChange={props.handleInputChange}
-  //                 name="firstname"
-  //               />
-  //             </Col>
-  //             <Col xs={6}>
-  //             <Form.Label>Last Name</Form.Label>
-  //               <Form.Control
-  //                 type="text"
-  //                 placeholder="Last name"
-  //                 value={props.userInfo.lastname}
-  //                 onChange={props.handleInputChange}
-  //                 name="lastname"
-  //               />
-  //             </Col>
-  //             </Row>
-  //             <Row className="my-2">
-  //             <Col xs={6}>
-  //             <Form.Label>Youre Email</Form.Label>
-  //               <Form.Control
-  //                 type="email"
-  //                 placeholder="Email"
-  //                 value={props.userInfo.email}
-  //                 onChange={props.handleInputChange}
-  //                 name="email"
-  //               />
-  //             </Col>
-  //             <Col xs={6}>
-  //             <Form.Label>Your Web Site</Form.Label>
-  //               <Form.Control
-  //                 type="text"
-  //                 placeholder="WebAddress"
-  //                 value={props.userInfo.website}
-  //                 onChange={props.handleInputChange}
-  //                 name="website"
-  //               />
-  //             </Col>
-  //           </Row>
-  //         </Form.Group>
-  //         <Form.Group controlId="exampleForm.ControlTextarea1">
-  //           <Form.Label>Blurb</Form.Label>
-  //           <Form.Control
-  //             type="text"
-  //             as="textarea"
-  //             rows="3"
-  //             placeholder="Pithy Statement 50 Characters or less"
-  //             maxLength="50"
-  //             value={props.userInfo.blurb}
-  //             onChange={props.handleInputChange}
-  //             name="blurb"
-  //           />
-  //         </Form.Group>
-  //         <Button
-  //           variant="primary"
-  //           type="submit"
-  //           // onClick={props.handleFormSubmit}
-  //           onClick={(e) => saveButton(e)}
-  //         >
-  //           Save!
-  //         </Button>
-  //       </Form>
-  //     </>
-  //   );
-  // };
-
   export function ItemForm (props) {
 
     function saveButton(e){
@@ -104,17 +19,43 @@ import Button from "react-bootstrap/Button";
     return (
       <Form>
         <Form.Group controlId="porfolioInfoForm">
-          <Form.Label>Portfolio Info:</Form.Label>
-          <Form.Control
-            type="text"
-            as="textarea"
-            rows="3"
-            placeholder="Info about your art"
-            value={props.portfolioInfo}
-            onChange={props.handleInputChange}
-            name="portfolioInfo"
-          />
+        <Row className="my-2">
+              <Col xs={12}>
+              <Form.Label>Image Link</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="image url"
+                  value={props.item.imageUrl}
+                  onChange={props.handleInputChange}
+                  name="imageUrl"
+                />
+              </Col>
+              <Col xs={12}>
+              <Form.Label>Item Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Title of this Item"
+                  value={props.item.imageTitle}
+                  onChange={props.handleInputChange}
+                  name="imageTitle"
+                />
+              </Col>
+              </Row>
+              
         </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Info About Porfolio Item: </Form.Label>
+            <Form.Control
+              type="text"
+              as="textarea"
+              rows="3"
+              placeholder="Info about this Item: "
+              maxLength="300"
+              value={props.item.imageAbout}
+              onChange={props.handleInputChange}
+              name="imageAbout"
+            />
+          </Form.Group>
         <Button
           variant="primary"
           type="submit"
