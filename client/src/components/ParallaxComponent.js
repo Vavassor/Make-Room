b748d16bf0307818fbd3bd89bb1731d87d2dfda8
Zkinsk <zoolunk@gmail.com>
2019-04-28 00:00:57 -0400

    adds a few more background images

--- client/src/components/ParallaxComponent.js
+++ client/src/components/ParallaxComponent.js
@@ -6,22 +6,12 @@ const styles = {
 };
 
 
-const imageList = [
-  "/images/background-1.jpg",
-  "/images/background-2.jpg",
-  "/images/background-3.jpg",
-  "/images/background-4.jpg",
-  "/images/background-5.jpg",
-  "/images/background-6.jpg",
-  "/images/background-7.jpg",
-  "/images/background-8.jpg",
-  "/images/background-9.jpg",
-]
 
 function randomImage() {
-  let i = imageList.length;
-  let rand = Math.floor(Math.random() * i);
-  return imageList[rand];
+  let i = 16;
+  let rand = Math.floor(Math.random() * i ) + 1;
+  return `/images/background-${rand}.jpg`;
+  // return imageList[rand];
 }
 
 
--- client/src/utilities/randomBgImages.js
+++ client/src/utilities/randomBgImages.js
@@ -8,6 +8,13 @@ const imageList = [
   "/images/background-7.jpg",
   "/images/background-8.jpg",
   "/images/background-9.jpg",
+  "/images/background-10.jpg",
+  "/images/background-11.jpg",
+  "/images/background-12.jpg",
+  "/images/background-13.jpg",
+  "/images/background-14.jpg",
+  "/images/background-15.jpg",
+  "/images/background-16.jpg",
 ]
 
 module.exports=imageList
